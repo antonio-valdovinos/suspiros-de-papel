bccbb1c4fc60b7c91e8f8d9190632d80ce23011b
Jesus Antonio Valdovinos Mendez <antoniovaldovinos.dev@gmail.com>
2026-04-07 15:58:10 -0600

    UI Container created

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -1,17 +1,20 @@
 import HeroContent from "./HeroContent"
 import HeroImage from "./HeroImage"
 import HeroBackground from "./HeroBackground"
+import Container from "@/components/ui/Container"
 
 export default function Hero() {
   return (
     <section className="relative min-h-[90vh] flex items-center overflow-hidden px-8 py-20">
-      
+
       <HeroBackground />
 
-      <div className="max-w-7xl mx-auto w-full grid lg:grid-cols-12 gap-8 items-center relative z-10">
-        <HeroContent />
-        <HeroImage />
-      </div>
+      <Container>
+        <div className="w-full grid lg:grid-cols-12 gap-8 items-center relative z-10">
+          <HeroContent />
+          <HeroImage />
+        </div>
+      </Container>
 
     </section>
   )
